74719c4d4eaba5a48d824825cdc275744f847ce3
tiger <xiawh@sugon.com>
2021-07-02 16:21:15 +0800

    [tiger] change to knife4j

---
+++ src/main/java/com/sugon/cloud/document/controller/ContentController.java
@@ -0,0 +1,13 @@
+package com.sugon.cloud.document.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Api(tags = "文档内容接口")
+@RestController
+@RequestMapping("/content")
+public class ContentController {
+}
