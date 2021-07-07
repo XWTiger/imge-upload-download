9fced5d05d7f72a23d2b7859c7ac9b65858e1feb
xiejy <xjy13141519>
2021-07-07 10:42:24 +0800

    health controller add tag

--- src/main/java/com/sugon/cloud/document/controller/HeathyController.java
+++ src/main/java/com/sugon/cloud/document/controller/HeathyController.java
@@ -1,11 +1,13 @@
 package com.sugon.cloud.document.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/micro-healthy")
+@Api(tags = {"Doc健康检查操作接口"},description = " ")
 public class HeathyController {
 
     @GetMapping
