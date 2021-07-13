16dbe1c8d01fb74390b522d750ee678742045e01
tiger <xiawh@sugon.com>
2021-07-13 11:25:59 +0800

    [tiger]fix catalog update

--- src/main/java/com/sugon/cloud/document/server/impl/ModuleServerImpl.java
+++ src/main/java/com/sugon/cloud/document/server/impl/ModuleServerImpl.java
@@ -17,10 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class ModuleServerImpl implements ModuleServer {
