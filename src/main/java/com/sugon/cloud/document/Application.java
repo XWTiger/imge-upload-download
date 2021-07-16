d5b836cc384b096f3d8830d597252a3b3767a8bf
tiger <xiawh@sugon.com>
2021-07-16 11:39:00 +0800

    [tiger] use nacos

--- src/main/java/com/sugon/cloud/document/Application.java
+++ src/main/java/com/sugon/cloud/document/Application.java
@@ -3,10 +3,12 @@ package com.sugon.cloud.document;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableDiscoveryClient
 @MapperScan("com.sugon.cloud.document.mapper")
 public class Application {
 
